fc820931f35c45c76ee12fa10aa86b0b8ef6a25f
Miyuki Koshiba <ksbmyk@gmail.com>
2023-05-31 19:26:16 +0900

    stateを用意して初期値を与えて表示

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+    const [menbers, setMenbers] = useState(['Aチーム', 'Bチーム']);
+    const [shuffleMenbers, setShuffleMenbers] = useState(['Bチーム', 'Aチーム']);
     return (
         <>
             <div className="input-area">
                 <input placeholder="なまえ"></input>
-                <button>エントリー</button>
+                <button id="add-button">エントリー</button>
             </div>
             <div className="member-area">
                 <div className='list-row'>
@@ -13,29 +16,28 @@ const App = () => {
                     <button>リセット</button>
                 </div>
                 <ul>
-                    <li>
-                        <div className='list-row'>
-                            <p>Aチーム</p>
-                            <button>やっぱやめ</button>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='list-row'>
-                            <p>Bチーム</p>
-                            <button>やっぱやめ</button>
-                        </div>
-                    </li>
+                    {menbers.map((menber) => {
+                        return (
+                            <li key={menber}>
+                                <div className='list-row'>
+                                    <p>{menber}</p>
+                                    <button>やっぱやめ</button>
+                                </div>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
             <div className='member-area'>
                 <p className='title'>順番</p>
                 <ul>
-                    <li>
-                        <p>1. Bチーム</p>
-                    </li>
-                    <li>
-                        <p>2. Aチーム</p>
-                    </li>
+                    {shuffleMenbers.map((menber, index) => {
+                        return (
+                        <li key={shuffleMenbers}>
+                            <p>{index + 1}. {menber}</p>
+                        </li>
+                        );
+                    })}
                 </ul>
             </div>
         </>
